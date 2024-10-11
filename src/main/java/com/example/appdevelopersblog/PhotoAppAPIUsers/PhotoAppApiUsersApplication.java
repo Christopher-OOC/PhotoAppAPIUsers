956fb06e74f6378d4b9404f4c1fe5ae855d68b2a
Christopher-OOC <olojedechristopher24@gmail.com>
2024-10-11 22:12:16 +0100

    vodmd

--- src/main/java/com/example/appdevelopersblog/PhotoAppAPIUsers/PhotoAppApiUsersApplication.java
+++ src/main/java/com/example/appdevelopersblog/PhotoAppAPIUsers/PhotoAppApiUsersApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class PhotoAppApiUsersApplication {
-	//grt
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhotoAppApiUsersApplication.class, args);
